db54a794b1088eb5d39251d4987da914459976af
Denis <gorodetsky.denis@gmail.com>
2019-10-23 11:50:46 +0300

    adaptive commit

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -2,23 +2,22 @@ const paralax = document.querySelector('.parallax');
 const layers = paralax.children;
 
 function moveLayersDependOnScroll(wScroll) {
-    Array.from(layers).forEach(layer => {
-        let divider = layer.dataset.speed;
-        
-        if (divider == 0.2) {    // увеличим скорость шара        
-            divider = 3;            
-        }
+	Array.from(layers).forEach(layer => {
+		let divider = layer.dataset.speed;
 
-        const strafe = wScroll * divider / 37; // strafe - разнести              
+		if (divider == 0.2) { // увеличим скорость шара        
+			divider = 3;
+		}
 
-        if (divider < 0.6 || divider >= 2) { // не фон и не передние облака
-            layer.style.transform = `translateY(-${strafe}%)`;
-        }        
-    });    
+		const strafe = wScroll * divider / 37; // strafe - разнести              
+
+		if (divider < 0.6 || divider >= 2) { // не фон и не передние облака
+			layer.style.transform = `translateY(-${strafe}%)`;
+		}
+	});
 }
 
-window.addEventListener('scroll', e=> {    
-    const wScroll = window.pageYOffset; // смещение по скролу
-    moveLayersDependOnScroll(wScroll);
+window.addEventListener('scroll', e => {
+	const wScroll = window.pageYOffset; // смещение по скролу
+	moveLayersDependOnScroll(wScroll);
 });
-
--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -2,33 +2,42 @@ import Vue from 'vue';
 import Flickity from 'vue-flickity';
 
 new Vue({
-    el:"#review",
+	el:"#review",
 
-    components: {
-        Flickity
-    },
+	components: {
+		Flickity
+	},
 
-    data () {
-        return {
-            flickityOptions: {
-                initialIndex: 3,
-                prevNextButtons: false,
-                pageDots: false,
-                wrapAround: true,
-                contain: true
+	data () {
+		return {
+			reviews: [],
+			flickityOptions: {
+				initialIndex: 1,
+				prevNextButtons: false,
+				pageDots: false,
+				groupCells: 2
+			}
+		}
+	},
 
-                // any options from Flickity can be used
-            }
-        }
-    },
+	methods: {
+		next() {
+			this.$refs.flickity.next();
+		},
 
-    methods: {
-        next() {
-            this.$refs.flickity.next();
-        },
-
-        previous() {
-            this.$refs.flickity.previous();
-        }
-    }
+		previous() {
+			this.$refs.flickity.previous();
+		},
+		arrWithImg(array) {
+			return array.map(item => {
+				const pic = require(`images/content/reviews/${item.pic}`);
+				item.pic = pic;
+				return item;
+			})
+		}
+	},
+	created() {
+		const data = require("../data/reviews.json");
+		this.reviews = this.arrWithImg(data);
+	}
 });
